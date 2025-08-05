655cb6a45dcfbd1b22937d1c23413f825c8bf8e6
TheWildJames <bins4us@hotmail.com>
2025-08-05 01:08:31 -0400

    Fix missing dp import in Customization.kt
    - Added missing androidx.compose.ui.unit.dp import
    - Fixes compilation errors for transparency slider UI elements

--- manager/app/src/main/java/com/rifsxd/ksunext/ui/screen/Customization.kt
+++ manager/app/src/main/java/com/rifsxd/ksunext/ui/screen/Customization.kt
@@ -57,6 +57,7 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.dropUnlessResumed
 import com.maxkeppeker.sheets.core.models.base.Header
 import com.maxkeppeker.sheets.core.models.base.rememberUseCaseState
