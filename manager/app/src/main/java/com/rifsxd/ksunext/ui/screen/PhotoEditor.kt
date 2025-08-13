dcbd921e1d406bff1774e2fffb07d71782ba9f46
TheWildJames <bins4us@hotmail.com>
2025-08-13 02:38:53 -0400

    Remove remaining hideControls parameter from PhotoEditor function call

--- manager/app/src/main/java/com/rifsxd/ksunext/ui/screen/PhotoEditor.kt
+++ manager/app/src/main/java/com/rifsxd/ksunext/ui/screen/PhotoEditor.kt
@@ -101,9 +101,6 @@ fun PhotoEditorScreen(
             },
             onProvideSaveFunction = { saveFunc ->
                 saveFunction = saveFunc
-            },
-            onProvideHideControlsFunction = { hideFunc ->
-                hideControlsFunction = hideFunc
             }
         )
     }
