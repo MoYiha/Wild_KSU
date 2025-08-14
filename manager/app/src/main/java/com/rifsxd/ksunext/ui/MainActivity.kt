d302e6976a98424c5572b57876ce03c1846552a7
TheWildJames <bins4us@hotmail.com>
2025-08-14 03:56:34 -0400

    Hide bottom bar in PhotoEditor for full screen experience

--- manager/app/src/main/java/com/rifsxd/ksunext/ui/MainActivity.kt
+++ manager/app/src/main/java/com/rifsxd/ksunext/ui/MainActivity.kt
@@ -407,6 +407,7 @@ class MainActivity : ComponentActivity() {
                 val showBottomBar = when (currentDestination?.route) {
                     FlashScreenDestination.route -> false // Hide for FlashScreenDestination
                     ExecuteModuleActionScreenDestination.route -> false // Hide for ExecuteModuleActionScreen
+                    PhotoEditorScreenDestination.route -> false // Hide for PhotoEditorScreen
                     else -> true
                 }
 
