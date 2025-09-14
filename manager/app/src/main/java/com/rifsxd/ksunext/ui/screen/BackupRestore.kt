ff4ca59d58d941a5f5f692fc867c3a6c325fa6bb
TheWildJames <bins4us@hotmail.com>
2025-09-14 01:07:42 -0400

    fix: add missing imports for clip and dp in AppProfile and BackupRestore

--- manager/app/src/main/java/com/rifsxd/ksunext/ui/screen/BackupRestore.kt
+++ manager/app/src/main/java/com/rifsxd/ksunext/ui/screen/BackupRestore.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.dropUnlessResumed
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
